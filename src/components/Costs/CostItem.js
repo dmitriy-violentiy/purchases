82f8ecafcdf5931a5a16cc97de3ee3a17699fdf4
violentiy1997 <violentiy1997@mail.ru>
2023-08-22 11:54:31 +0300

    add state in project

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,17 +1,24 @@
 import Card from "../UI/Card";
 import CostDate from "./CostDate";
 import "./CostItem.css";
+import React, { useState } from "react";
 
 const CostItem = (props) => {
+	const [description, setDescription] = useState(props.description);
+
+	const changeDescription = () => {
+		setDescription("New text");
+	};
 	return (
 		<Card className="cost-item">
 			<CostDate date={props.date} />
 			<div className="cost-item__description">
-				<h2>{props.description}</h2>
+				<h2>{description}</h2>
 				<div className="cost-item__price">{props.amount}р.</div>
 			</div>
+			<button onClick={changeDescription}>Изменить описание</button>
 		</Card>
 	);
-}
+};
 
 export default CostItem;
