fff62d00c5dd6bf6ca4ba2e1c86a2e0212c59ef6
violentiy1997 <violentiy1997@mail.ru>
2023-08-23 15:13:59 +0300

    added the ability to add cost

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import Costs from "./components/Costs/Costs";
 import NewCost from "./components/NewCost/NewCost";
+import React, { useState } from "react";
+
+const INITIAL_COSTS = [
+	{
+		id: "c1",
+		date: new Date(2021, 2, 12),
+		description: "Холодильник",
+		amount: 70000,
+	},
+	{
+		id: "c2",
+		date: new Date(2021, 11, 25),
+		description: "Айфон",
+		amount: 120000,
+	},
+	{
+		id: "c3",
+		date: new Date(2021, 4, 1),
+		description: "Табурет",
+		amount: 3000,
+	},
+];
 
 const App = () => {
-	const costs = [
-		{
-			id: "c1",
-			date: new Date(2021, 2, 12),
-			description: "Холодильник",
-			amount: 70000,
-		},
-		{
-			id: "c2",
-			date: new Date(2021, 11, 25),
-			description: "Айфон",
-			amount: 120000,
-		},
-		{
-			id: "c3",
-			date: new Date(2021, 4, 1),
-			description: "Табурет",
-			amount: 3000,
-		},
-	];
+	const [costs, setCosts] = useState(INITIAL_COSTS);
 
 	const addCostHandler = (cost) => {
-		console.log(cost);
-		console.log("app");
+		setCosts((prevCosts) => {
+			return [cost, ...prevCosts];
+		});
 	};
 
 	return (
