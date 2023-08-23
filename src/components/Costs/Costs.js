2d5ce47454e3f1ac543e3201c1af9de56b5bdee2
violentiy1997 <violentiy1997@mail.ru>
2023-08-23 15:50:54 +0300

    add filter in Coasts

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -9,12 +9,17 @@ const Costs = (props) => {
 		setSelectedYear(year);
 	};
 
+   const filteredCoasts = props.costs.filter(cost => {
+      return cost.date.getFullYear().toString() === selectedYear
+   })
+
 	return (
 		<div>
 			<CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
 			<div className="costs">
-				{props.costs.map((cost) => (
+				{filteredCoasts.map((cost) => (
 					<CostItem
+                  key={cost.id}
 						date={cost.date}
 						description={cost.description}
 						amount={cost.amount}
