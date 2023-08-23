35ec97e805170bcb3d59ffdff2c54674c888d03d
violentiy1997 <violentiy1997@mail.ru>
2023-08-23 10:33:31 +0300

    add costData on App.js

--- src/App.js
+++ src/App.js
@@ -4,25 +4,33 @@ import NewCost from "./components/NewCost/NewCost";
 const App = () => {
 	const costs = [
 		{
+			id: "c1",
 			date: new Date(2021, 2, 12),
 			description: "Холодильник",
 			amount: 70000,
 		},
 		{
+			id: "c2",
 			date: new Date(2021, 11, 25),
 			description: "Айфон",
 			amount: 120000,
 		},
 		{
+			id: "c3",
 			date: new Date(2021, 4, 1),
 			description: "Табурет",
 			amount: 3000,
 		},
 	];
 
+	const addCostHandler = (cost) => {
+		console.log(cost);
+		console.log("app");
+	};
+
 	return (
 		<div>
-			<NewCost />
+			<NewCost onAddCost={addCostHandler} />
 			<Costs costs={costs} />
 		</div>
 	);
