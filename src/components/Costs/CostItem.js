e938715b800031234c0682979edd36589ba1353b
violentiy1997 <violentiy1997@mail.ru>
2023-08-23 16:25:01 +0300

    create component CostList

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -5,13 +5,15 @@ import React, { useState } from "react";
 
 const CostItem = (props) => {
 	return (
-		<Card className="cost-item">
-			<CostDate date={props.date} />
-			<div className="cost-item__description">
-				<h2>{props.description}</h2>
-				<div className="cost-item__price">{props.amount}р.</div>
-			</div>
-		</Card>
+		<li>
+			<Card className="cost-item">
+				<CostDate date={props.date} />
+				<div className="cost-item__description">
+					<h2>{props.description}</h2>
+					<div className="cost-item__price">{props.amount}р.</div>
+				</div>
+			</Card>
+		</li>
 	);
 };
 
--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,6 +1,6 @@
-import CostItem from "./CostItem";
 import CostsFilter from "./CostsFilter";
 import React, { useState } from "react";
+import CostList from "./CostList"
 
 const Costs = (props) => {
 	const [selectedYear, setSelectedYear] = useState("2020");
@@ -9,23 +9,15 @@ const Costs = (props) => {
 		setSelectedYear(year);
 	};
 
-   const filteredCoasts = props.costs.filter(cost => {
-      return cost.date.getFullYear().toString() === selectedYear
-   })
+	const filteredCoasts = props.costs.filter((cost) => {
+		return cost.date.getFullYear().toString() === selectedYear;
+	});
 
+	
 	return (
 		<div>
 			<CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
-			<div className="costs">
-				{filteredCoasts.map((cost) => (
-					<CostItem
-                  key={cost.id}
-						date={cost.date}
-						description={cost.description}
-						amount={cost.amount}
-					/>
-				))}
-			</div>
+			<CostList costs={filteredCoasts}/>
 		</div>
 	);
 };
